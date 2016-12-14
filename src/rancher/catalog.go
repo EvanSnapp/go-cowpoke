e70f98e59df68ae3cae26d1b8c9e718d95b6bdeb
shawn hartsell <shawn.hartsell@leankit.com>
2016-12-14 09:44:20 -0600

    refactoring functions that retrive rancher catalogs

--- src/rancher/catalog.go
+++ src/rancher/catalog.go
@@ -5,50 +5,49 @@ import (
 	"net/url"
 	"os"
 	"rancher/types"
+	"reflect"
 )
 
 //GetTemplateURL returns the URL associated with a catalog template at the specified version
-func GetTemplateURL(catalog string, template string, version string) (*url.URL, error) {
+func GetTemplateURL(catalog string, template string, version string) (string, error) {
 
-	var data types.Template
+	var data *types.Template
+	var err error
 	catalogID := fmt.Sprintf("%s:%s", url.PathEscape(catalog), url.PathEscape(template))
 	catalogURL := os.Getenv("RANCHER_URL") + "/v1-catalog/templates/" + catalogID
 
-	if err := DoRequest(catalogURL, &data); err != nil {
-		return nil, err
+	if e := DoRequest(catalogURL, &data); err != nil {
+		err = e
 	}
 
-	if val, found := data.VersionLinks[version]; found {
-		//paranoia check...make sure that the found value is a string
-		if templateVersionURL, isString := val.(string); isString {
-			//putting on the tin foil hat and ensuring that the string is actually a URL
-			if url, err := url.Parse(templateVersionURL); err == nil {
-				return url, nil
+	templateURL, found := data.VersionLinks[version]
 
-			}
-			return nil, ErrServer
-		}
-
-		return nil, ErrServer
+	if !found {
+		err = ErrNotFound
 	}
 
-	return nil, ErrNotFound
+	return templateURL, ErrNotFound
 }
 
 //GetTemplateVersion will retrieve the rancher and docker information for a catalog template
 //at the specified version.
 func GetTemplateVersion(catalog string, template string, version string) (*types.TemplateVersion, error) {
 	var data *types.TemplateVersion
+	var err error
 
-	templateURL, err := GetTemplateURL(catalog, template, version)
+	templateURL, e := GetTemplateURL(catalog, template, version)
 
 	if err != nil {
-		return nil, err
+		err = e
 	}
 
-	if err := DoRequest(templateURL.String(), &data); err != nil {
-		return nil, err
+	if e2 := DoRequest(templateURL, &data); e2 != nil {
+		err = e2
 	}
 
-	return data, nil
+	if (data == nil || reflect.DeepEqual(data, types.TemplateVersion{})) {
+		err = ErrNotFound
+	}
+
+	return data, err
 }
--- src/rancher/types/template.go
+++ src/rancher/types/template.go
@@ -2,17 +2,17 @@ package types
 
 //Template represents catalog template data retrived from the Rancher API
 type Template struct {
-	Name         string                 `json:"name"`
-	ID           string                 `json:"id"`
-	VersionLinks map[string]interface{} `json:"versionLinks"`
+	Name         string            `json:"name"`
+	ID           string            `json:"id"`
+	VersionLinks map[string]string `json:"versionLinks"`
 }
 
 //TemplateVersion represents catalog template version data retrieved from the Rancher API
 type TemplateVersion struct {
-	ID             string                 `json:"id"`
-	Name           string                 `json:"name"`
-	CatalogID      string                 `json:"catalogId"`
-	Version        string                 `json:"version"`
-	DefaultVersion string                 `json:"defaultVersion"`
-	TemplateFiles  map[string]interface{} `json:"files"`
+	ID             string            `json:"id"`
+	Name           string            `json:"name"`
+	CatalogID      string            `json:"catalogId"`
+	Version        string            `json:"version"`
+	DefaultVersion string            `json:"defaultVersion"`
+	TemplateFiles  map[string]string `json:"files"`
 }
